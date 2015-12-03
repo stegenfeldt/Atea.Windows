eaa60d244ec0daed5b9aaf19b8912b99789e2cfa
stegenfeldt <samuel.tegenfeldt@atea.se>
2015-12-03 10:35:45 +0100

    Incorporated scripts and fragments from Atea.WinSvc. Needs refactoring of class names.

---
+++ Atea.Windows.Service.Monitoring/Scripts/Add-MonitoredServiceKey.ps1
@@ -0,0 +1,9 @@
+﻿param(
+	[string] $serviceName,
+	[string] $registryPath = "SOFTWARE\Atea\WinSvc"
+)
+
+$registryPath = "HKLM:\$registryPath"
+
+New-Item $registryPath -Force
+New-ItemProperty -Path $registryPath -Name $serviceName -PropertyType String -Force
