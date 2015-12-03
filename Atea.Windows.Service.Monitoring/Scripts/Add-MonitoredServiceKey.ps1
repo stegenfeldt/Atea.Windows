fc8c474c72491ec9879f56339bf45e66e386ad96
stegenfeldt <samuel.tegenfeldt@atea.se>
2015-12-03 15:21:51 +0100

    Scripts, fragments, classes etc have been refactored to fit the Atea.Windows.Service... naming convention.

--- Atea.Windows.Service.Monitoring/Scripts/Add-MonitoredServiceKey.ps1
+++ Atea.Windows.Service.Monitoring/Scripts/Add-MonitoredServiceKey.ps1
@@ -5,5 +5,10 @@
 
 $registryPath = "HKLM:\$registryPath"
 
-New-Item $registryPath -Force
+if (!(Test-Path $registryPath)) {
+	Write-Host ("{0} does not exist, will create key." -f $registryPath)
+	New-Item $registryPath -Force
+}
 New-ItemProperty -Path $registryPath -Name $serviceName -PropertyType String -Force
+
+Write-Host "`nNew service key added`n`nPlease wait for next discovery, `nor restart the Microsoft Monitoring agent to force a discovery."
