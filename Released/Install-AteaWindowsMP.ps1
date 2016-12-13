a8b49cf312f7fdd4778511b6df0e0ea74c0a0eb9
Samuel Tegenfeldt <samuel.tegenfeldt@atea.se>
2016-12-13 13:53:47 +0100

    Install script should try to uninstall current version now

--- Released/Install-AteaWindowsMP.ps1
+++ Released/Install-AteaWindowsMP.ps1
@@ -1,6 +1,34 @@
+<#
+.Synopsis
+Downloads, installs and optionally imports the latest builds of the Atea.Windows MP package.
+
+.Parameter Version
+Select which build to install. 
+"Stable" will download from the master branch (tested and true)
+"Latest" will download from the develop branch (latest build)
+
+.Parameter ImportMP
+Will attempt to import installed MPs into SCOM. 
+This will likely fail if you move from "Latest" to "Stable" version as the prior tend to have a higher version number.
+
+.Parameter OMMSComputerName
+Defaults to the local computer. Set this to connect to a different SCOM Management Server for the import.
+
+.Example
+# Download, install and import the latest build on the local management server
+.\Install-AteaWindowsMP.ps1 -Version Latest -ImportMP
+
+.Example
+# Download and install latest version locally, import to a different SCOM Management Server
+.\Install-AteaWindowsMP.ps1 -Version Latest -ImportMP -OMMSComputerName <omms1.domain.fqdn>
+
+.Example
+# Download and install stable version locally. Don't import.
+.\Install-AteaWindowsMP.ps1 -Version Stable
+#>
+[CmdletBinding()]
 param(
-    [switch] $Stable,
-    [switch] $Latest,
+    [ValidateSet("Stable","Latest")][string] $Version,
     [switch] $ImportMP,
     [string] $OMMSComputerName = "."
 )
@@ -11,11 +39,15 @@ param(
 [string] $installDir = "$(${env:ProgramFiles(x86)})\System Center Management Packs\Atea.Windows\"
 [string] $msiFilePath = "$($env:TEMP)\Atea.Windows.Installer.msi"
 
+[string] $packageName = "Atea Windows - MP Package"
+
 if ($Stable) {
    Invoke-WebRequest -Uri $masterURL -OutFile $msiFilePath
+   if (Get-Package -Name $packageName -ErrorAction SilentlyContinue) {Uninstall-Package -Name $packageName -Force}
    Install-Package -Name $msiFilePath -Force
 } elseif ($Latest) {
    Invoke-WebRequest -Uri $developURL -OutFile $msiFilePath
+   if (Get-Package -Name $packageName -ErrorAction SilentlyContinue) {Uninstall-Package -Name $packageName -Force}
    Install-Package -Name $msiFilePath -Force
 }
 
