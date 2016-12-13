97dd238751912cb56db1765c8898cd5509e0970f
Samuel Tegenfeldt <samuel.tegenfeldt@atea.se>
2016-12-13 13:21:23 +0100

    Fixed C#-like function calls, since this is PoSH and not C#

--- Atea.Windows.Service.Monitoring/Scripts/Restart-MonitoredService.ps1
+++ Atea.Windows.Service.Monitoring/Scripts/Restart-MonitoredService.ps1
@@ -24,7 +24,7 @@ param(
 function Write-SCOMEvent
 {
 	param(
-		[string] $LogMessage,
+		[Parameter(Mandatory=$true)][string] $LogMessage,
 		[ValidateSet(0,1,2)][int] $LogSeverity,
 		[ValidateRange(0,20000)][int] $LogEventId
 	)
@@ -46,10 +46,10 @@ function Test-ServiceExist ($ServiceName) {
 
 function Start-ServiceAndWait ([string] $ServiceName, [int] $WaitSeconds) {
 	Write-Host "Attempting to restart $ServiceName"
-	Write-SCOMEvent "Attempting to restart $ServiceName", 0,10101
+	Write-SCOMEvent -LogMessage "Attempting to restart $ServiceName" -LogSeverity 0 -LogEventId 10101
 	Restart-Service -Name $ServiceName
 	Write-Host "Waiting for $WaitSeconds seconds to verify service state..."
-	Write-SCOMEvent "Waiting for $WaitSeconds seconds to verify `"$ServiceName`" state...", 0,10101
+	Write-SCOMEvent -LogMessage "Waiting for $WaitSeconds seconds to verify `"$ServiceName`" state..." -LogSeverity 0 -LogEventId 10101
 	Start-Sleep -Seconds $WaitSeconds
 	$serviceObject = Get-Service -Name $ServiceName
 	return [string] $serviceObject.Status
@@ -59,24 +59,24 @@ function Restart-ServiceWithRepeats ([int] $MaxRepeatCount, [int] $RepeatInterva
 	if ($MaxRepeatCount -gt 0) {
 		for ($i = 1; $i -le $MaxRepeatCount; $i++) {
 			Write-Host "Trying again ($i of $MaxRepeatCount)"
-			Write-SCOMEvent "Trying to restart `'$ServiceName`' again ($i of $MaxRepeatCount)",0,10101
+			Write-SCOMEvent -LogMessage "Trying to restart `'$ServiceName`' again ($i of $MaxRepeatCount)" -LogSeverity 0 -LogEventId 10101
 			[string] $serviceStatus = Start-ServiceAndWait -ServiceName $ServiceName -WaitSeconds $Delay
 
 			if ($serviceStatus -ne "Running") {
 				if ($i -eq $MaxRepeatCount) {
 					Write-Host "Service is still not running. This was the last attempt!" -ForegroundColor DarkRed
-					Write-SCOMEvent "`'$ServiceName`' is still not running. This was the last attempt!", 0, 10101
-					Write-SCOMEvent "Failed to recover service `'$ServiceName`' after $MaxRepeatCount attempts!", 1, 10100
+					Write-SCOMEvent -LogMessage "`'$ServiceName`' is still not running. This was the last attempt!" -LogSeverity 0 -LogEventId 10101
+					Write-SCOMEvent -LogMessage "Failed to recover service `'$ServiceName`' after $MaxRepeatCount attempts!" -LogSeverity 1 -LogEventId 10100
 				} else {
 					Write-Host "Service is still not running, waiting $RepeatIntervalSeconds before next try..."
-					Write-SCOMEvent "`'$ServiceName`' is still not running, waiting $RepeatIntervalSeconds before next try...",0,10101
+					Write-SCOMEvent -LogMessage "`'$ServiceName`' is still not running, waiting $RepeatIntervalSeconds before next try..." -LogSeverity 0 -LogEventId 10101
 				}
 				Start-Sleep -Seconds $RepeatIntervalSeconds
 			} else {
 				# Service has started and has been running for $Delay seconds
 				$i = $MaxRepeatCount
 				Write-Host "`'$ServiceName`' is now running!" -ForegroundColor Green
-				Write-SCOMEvent "`'$ServiceName`' is now running!", 0, 10101
+				Write-SCOMEvent -LogMessage "`'$ServiceName`' is now running!" -LogSeverity 0 -LogEventId 10101
 			}
 		}
 	}
