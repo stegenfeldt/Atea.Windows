e4ecd54000955d6bda7ae00b9ce0b88df6712b01
Samuel Tegenfeldt <samuel.tegenfeldt@atea.se>
2019-04-01 15:58:15 +0200

    "Add Monitored EventLog" task complete

--- Atea.Windows.Server.Monitoring/Scripts/Add-MonitoredWindowsEventKey.ps1
+++ Atea.Windows.Server.Monitoring/Scripts/Add-MonitoredWindowsEventKey.ps1
@@ -1,10 +1,12 @@
 ﻿param(
-	[string] $EventMonitorName,
-	[string] $EventShortDescription,
-	[string] $errorEventId,
+	[string] $eventMonitorName,
+	[string] $eventShortDescription,
+	[int] $errorEventId,
+	[int] $errorEventLevel,
 	[string] $errorEventSource,
 	[string] $errorEventLog,
-	[string] $healthyEventId,
+	[int] $healthyEventId,
+	[int] $healthyEventLevel,
 	[string] $healthyEventSource,
 	[string] $healthyEventLog
 )
@@ -18,11 +20,13 @@ if (!(Test-Path $monitorRegistryPath)) {
 	New-Item $monitorRegistryPath -Force
 }
 New-ItemProperty -Path $monitorRegistryPath -Name "ErrorEventId" -Value $errorEventId -PropertyType String -Force
+New-ItemProperty -Path $monitorRegistryPath -Name "ErrorEventId" -Value $errorEventLevel -PropertyType int -Force
 New-ItemProperty -Path $monitorRegistryPath -Name "ErrorEventSource"  -Value $errorEventSource -PropertyType String -Force
 New-ItemProperty -Path $monitorRegistryPath -Name "ErrorEventLog"  -Value $errorEventLog -PropertyType String -Force
-New-ItemProperty -Path $monitorRegistryPath -Name "HealthyEventId"  -Value $healthyEventId -PropertyType String -Force
+New-ItemProperty -Path $monitorRegistryPath -Name "HealthyEventId"  -Value $healthyEventId -PropertyType int -Force
+New-ItemProperty -Path $monitorRegistryPath -Name "HealthyEventId"  -Value $healthyEventLevel -PropertyType int -Force
 New-ItemProperty -Path $monitorRegistryPath -Name "HealthyEventSource"  -Value $healthyEventSource -PropertyType String -Force
 New-ItemProperty -Path $monitorRegistryPath -Name "HealthyEventLog"  -Value $healthyEventLog -PropertyType String -Force
-New-ItemProperty -Path $monitorRegistryPath -Name "EventShortDescription"  -Value $EventShortDescription -PropertyType String -Force
+New-ItemProperty -Path $monitorRegistryPath -Name "EventShortDescription"  -Value $eventShortDescription -PropertyType String -Force
 
 Write-Output "`nNew service key added`n`nPlease wait for next discovery, `nor restart the Microsoft Monitoring agent to force a discovery."
