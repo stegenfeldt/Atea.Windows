dc2a9f80b3e0cf1202c6891086e6aad17016058e
Samuel Tegenfeldt <samuel.tegenfeldt@atea.se>
2019-04-01 14:59:09 +0200

    Added MonitoredEvent class and task-script

---
+++ Atea.Windows.Server.Monitoring/Scripts/Add-MonitoredWindowsEventKey.ps1
@@ -0,0 +1,28 @@
+﻿param(
+	[string] $EventMonitorName,
+	[string] $EventShortDescription,
+	[string] $errorEventId,
+	[string] $errorEventSource,
+	[string] $errorEventLog,
+	[string] $healthyEventId,
+	[string] $healthyEventSource,
+	[string] $healthyEventLog
+)
+
+[string] $registryPath = "SOFTWARE\CommunityMP\WinEvents"
+[string] $registryPath = "HKLM:\$registryPath"
+[string] $monitorRegistryPath = "$registryPath\$EventMonitorName"
+
+if (!(Test-Path $monitorRegistryPath)) {
+	Write-Output ("{0} does not exist, will create key." -f $monitorRegistryPath)
+	New-Item $monitorRegistryPath -Force
+}
+New-ItemProperty -Path $monitorRegistryPath -Name "ErrorEventId" -Value $errorEventId -PropertyType String -Force
+New-ItemProperty -Path $monitorRegistryPath -Name "ErrorEventSource"  -Value $errorEventSource -PropertyType String -Force
+New-ItemProperty -Path $monitorRegistryPath -Name "ErrorEventLog"  -Value $errorEventLog -PropertyType String -Force
+New-ItemProperty -Path $monitorRegistryPath -Name "HealthyEventId"  -Value $healthyEventId -PropertyType String -Force
+New-ItemProperty -Path $monitorRegistryPath -Name "HealthyEventSource"  -Value $healthyEventSource -PropertyType String -Force
+New-ItemProperty -Path $monitorRegistryPath -Name "HealthyEventLog"  -Value $healthyEventLog -PropertyType String -Force
+New-ItemProperty -Path $monitorRegistryPath -Name "EventShortDescription"  -Value $EventShortDescription -PropertyType String -Force
+
+Write-Output "`nNew service key added`n`nPlease wait for next discovery, `nor restart the Microsoft Monitoring agent to force a discovery."
