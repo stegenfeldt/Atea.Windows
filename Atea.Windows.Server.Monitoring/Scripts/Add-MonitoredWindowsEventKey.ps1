6356af60d9116efbbdf7cac7c11b0255f32860e9
stegenfeldt <stegenfeldt@outlook.com>
2019-04-04 15:42:28 +0200

    EventMonitor discovery script

--- Atea.Windows.Server.Monitoring/Scripts/Add-MonitoredWindowsEventKey.ps1
+++ Atea.Windows.Server.Monitoring/Scripts/Add-MonitoredWindowsEventKey.ps1
@@ -1,32 +1,33 @@
 ﻿param(
-	[string] $eventMonitorName,
-	[string] $eventShortDescription,
-	[int] $errorEventId,
-	[int] $errorEventLevel,
-	[string] $errorEventSource,
-	[string] $errorEventLog,
-	[int] $healthyEventId,
-	[int] $healthyEventLevel,
-	[string] $healthyEventSource,
-	[string] $healthyEventLog
+    [Parameter(Mandatory = 'true')][string] $eventMonitorName,
+    [Parameter(Mandatory = 'true')][string] $eventShortDescription,
+    [Parameter(Mandatory = 'true')][int] $errorEventId,
+    [Parameter(Mandatory = 'true')][int] $errorEventLevel,
+    [Parameter(Mandatory = 'true')][string] $errorEventSource,
+    [Parameter(Mandatory = 'true')][string] $errorEventLog,
+    [Parameter(Mandatory = 'true')][int] $healthyEventId,
+    [Parameter(Mandatory = 'true')][int] $healthyEventLevel,
+    [Parameter(Mandatory = 'true')][string] $healthyEventSource,
+    [Parameter(Mandatory = 'true')][string] $healthyEventLog
 )
 
-[string] $registryPath = "SOFTWARE\CommunityMP\WinEvents"
-[string] $registryPath = "HKLM:\$registryPath"
+[string] $registryPath = "HKLM:\SOFTWARE\CommunityMP\WinEvents"
 [string] $monitorRegistryPath = "$registryPath\$EventMonitorName"
 
 if (!(Test-Path $monitorRegistryPath)) {
-	Write-Output ("{0} does not exist, will create key." -f $monitorRegistryPath)
-	New-Item $monitorRegistryPath -Force
-}
-New-ItemProperty -Path $monitorRegistryPath -Name "ErrorEventId" -Value $errorEventId -PropertyType String -Force
-New-ItemProperty -Path $monitorRegistryPath -Name "ErrorEventId" -Value $errorEventLevel -PropertyType int -Force
-New-ItemProperty -Path $monitorRegistryPath -Name "ErrorEventSource"  -Value $errorEventSource -PropertyType String -Force
-New-ItemProperty -Path $monitorRegistryPath -Name "ErrorEventLog"  -Value $errorEventLog -PropertyType String -Force
-New-ItemProperty -Path $monitorRegistryPath -Name "HealthyEventId"  -Value $healthyEventId -PropertyType int -Force
-New-ItemProperty -Path $monitorRegistryPath -Name "HealthyEventId"  -Value $healthyEventLevel -PropertyType int -Force
-New-ItemProperty -Path $monitorRegistryPath -Name "HealthyEventSource"  -Value $healthyEventSource -PropertyType String -Force
-New-ItemProperty -Path $monitorRegistryPath -Name "HealthyEventLog"  -Value $healthyEventLog -PropertyType String -Force
-New-ItemProperty -Path $monitorRegistryPath -Name "EventShortDescription"  -Value $eventShortDescription -PropertyType String -Force
+    Write-Output ("{0} does not exist, will create key." -f $monitorRegistryPath)
+    New-Item $monitorRegistryPath -Force
+    New-ItemProperty -Path $monitorRegistryPath -Name "ErrorEventId" -Value $errorEventId -PropertyType DWord -Force
+    New-ItemProperty -Path $monitorRegistryPath -Name "ErrorEventId" -Value $errorEventLevel -PropertyType DWord -Force
+    New-ItemProperty -Path $monitorRegistryPath -Name "ErrorEventSource"  -Value $errorEventSource -PropertyType String -Force
+    New-ItemProperty -Path $monitorRegistryPath -Name "ErrorEventLog"  -Value $errorEventLog -PropertyType String -Force
+    New-ItemProperty -Path $monitorRegistryPath -Name "HealthyEventId"  -Value $healthyEventId -PropertyType DWord -Force
+    New-ItemProperty -Path $monitorRegistryPath -Name "HealthyEventId"  -Value $healthyEventLevel -PropertyType DWord -Force
+    New-ItemProperty -Path $monitorRegistryPath -Name "HealthyEventSource"  -Value $healthyEventSource -PropertyType String -Force
+    New-ItemProperty -Path $monitorRegistryPath -Name "HealthyEventLog"  -Value $healthyEventLog -PropertyType String -Force
+    New-ItemProperty -Path $monitorRegistryPath -Name "EventShortDescription"  -Value $eventShortDescription -PropertyType String -Force
 
-Write-Output "`nNew service key added`n`nPlease wait for next discovery, `nor restart the Microsoft Monitoring agent to force a discovery."
+    Write-Output "`nNew service key added`n`nPlease wait for next discovery, `nor restart the Microsoft Monitoring agent to force a discovery."
+} else {
+	Write-Output "`nKey already exists!`n`nLeaving as is."
+}
---
+++ Atea.Windows.Server.Monitoring/Scripts/Get-MonitoredEventDiscoveryPB.ps1
@@ -0,0 +1,47 @@
+### Gather vars and modules before script execution
+$startVars = (Get-Variable -Scope Global).Name; $startModules = Get-Module
+###
+
+[string] $registryPath = "HKLM:\SOFTWARE\CommunityMP\WinEvents"
+$omApi = New-Object -ComObject "MOM.ScriptAPI"
+if ((Get-WmiObject win32_computersystem).Domain -ne "WORKGROUP"){
+    $computerName = "$((Get-WmiObject win32_computersystem).DNSHostName).$((Get-WmiObject win32_computersystem).Domain)"
+} else {
+    $computerName = "$((Get-WmiObject win32_computersystem).DNSHostName)"
+}
+
+$discoData = $omApi.CreateDiscoveryData(0,'$MPElement$','$Target/Id$')
+
+if (Test-Path -Path $registryPath) {
+    $subKeys = Get-ChildItem -Path $registryPath
+    foreach ($subKey in $subKeys) {
+        $discoInstance = $discoData.CreateClassInstance('$MPElement[Name="Atea.Windows.Server.Monitoring.MonitoredEvent"]$')
+        $discoInstance.AddProperty('$MPElement[Name="Atea.Windows.Server.Monitoring.MonitoredEvent"]/EventMonitorName$',$subKey.PSChildName)
+        $discoInstance.AddProperty('$MPElement[Name="System!System.Entity"]/DisplayName$',$subKey.PSChildName)
+        $discoInstance.AddProperty('$MPElement[Name="Windows!Microsoft.Windows.Computer"]/PrincipalName$',$computerName)
+        foreach ($itemProperties in (Get-ItemProperty -Path $subKey.PSPath)) {
+            $discoInstance.AddProperty('$MPElement[Name="Atea.Windows.Server.Monitoring.MonitoredEvent"]/ErrorEventId$',$itemProperties.ErrorEventId)
+            $discoInstance.AddProperty('$MPElement[Name="Atea.Windows.Server.Monitoring.MonitoredEvent"]/ErrorEventLog$',$itemProperties.ErrorEventLog)
+            $discoInstance.AddProperty('$MPElement[Name="Atea.Windows.Server.Monitoring.MonitoredEvent"]/ErrorEventSource$',$itemProperties.ErrorEventSource)
+            $discoInstance.AddProperty('$MPElement[Name="Atea.Windows.Server.Monitoring.MonitoredEvent"]/HealthyEventId$',$itemProperties.HealthyEventId)
+            $discoInstance.AddProperty('$MPElement[Name="Atea.Windows.Server.Monitoring.MonitoredEvent"]/HealthyEventLog$',$itemProperties.HealthyEventLog)
+            $discoInstance.AddProperty('$MPElement[Name="Atea.Windows.Server.Monitoring.MonitoredEvent"]/HealthyEventSource$',$itemProperties.HealthyEventSource)
+            $discoInstance.AddProperty('$MPElement[Name="Atea.Windows.Server.Monitoring.MonitoredEvent"]/EventShortDescription$',$itemProperties.EventShortDescription)
+        }
+        $discoData.AddInstance($discoInstance)
+    }
+}
+$discoData
+
+### Unload generated vars and modules
+foreach ($module in (Get-Module)) {
+    if ($module -notin $startModules) {
+        Remove-Module $module
+    }
+}
+foreach ($var in (Get-Variable -Scope Global).Name) {
+    if ($var -notin $startVars) {
+        Remove-Variable $var
+    }
+}
+###
