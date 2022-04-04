f4bb168ffde94eefcb134bcf110ce83420b4da2c
Samuel Tegenfeldt <samuel.tegenfeldt@atea.se>
2022-04-04 19:21:37 +0200

    Updated error handling on Fileshare monitoring Fixes issue #47

--- Atea.Windows.File.Monitoring/Scripts/Get-FileShareSpacePB.ps1
+++ Atea.Windows.File.Monitoring/Scripts/Get-FileShareSpacePB.ps1
@@ -2,7 +2,7 @@
 param (
     [Parameter()]
     [string]
-    $UNCPath = '\\tgds01\web'
+    $UNCPath = '\\server\share'
     , [Parameter()]
     [string]
     $ShareName = 'A ShareName'
@@ -64,25 +64,38 @@ function Get-FileShareFreeSpace {
 
 
     if ($null -ne $drive) {
-        $usedGB = [math]::Round($($drive.Used / 1GB), 2) -as [double]
-        $freeGB = [math]::Round($($drive.Free / 1GB), 2) -as [double]
-        $totalGB = [math]::Round($usedGB + $freeGB, 2) -as [double]
+        if ($null -ne $drive.Used -and $drive.Used -ne 0) {
+            $usedGB = [math]::Round($($drive.Used / 1GB), 2) -as [double]
+        }
+        if ($null -ne $drive.Free) {
+            $freeGB = [math]::Round($($drive.Free / 1GB), 2) -as [double]
+        }
+        if ($null -ne $usedGB -and $null -ne $freeGB) {
+            $totalGB = [math]::Round($usedGB + $freeGB, 2) -as [double]
+        }
         if ($null -ne $totalGB -and $totalGB -ne 0) {
             $usedPct = [math]::Round(($usedGB / $totalGB) * 100, 2) -as [double]
             $freePct = [math]::Round(($freeGB / $totalGB) * 100, 2) -as [double]
         }
-        if ($null -eq $usedPct) {$usedPct = 0 -as [double]}
-        if ($null -eq $usedGB) {$usedGB = 0 -as [double]}
-        if ($null -eq $freePct) {$freePct = 0 -as [double]}
-        if ($null -eq $freeGB) {$freeGB = 0 -as [double]}
-        if ($null -eq $totalGB) {$totalGB = 0 -as [double]}
+        if ($null -eq $usedPct) { $usedPct = -1 -as [double] }
+        if ($null -eq $usedGB) { $usedGB = -1 -as [double] }
+        if ($null -eq $freePct) { $freePct = -1 -as [double] }
+        if ($null -eq $freeGB) { $freeGB = -1 -as [double] }
+        if ($null -eq $totalGB) { $totalGB = -1 -as [double] }
         $driveData = @{
-            'Path'    = $UNCPath
-            'TotalGB' = $totalGB
-            'UsedGB'  = $usedGB
-            'UsedPct' = $usedPct
-            'FreeGB'  = $freeGB
-            'FreePct' = $freePct
+            'ShareName' = $ShareName
+            'Path'      = $UNCPath
+            'TotalGB'   = $totalGB
+            'UsedGB'    = $usedGB
+            'UsedPct'   = $usedPct
+            'FreeGB'    = $freeGB
+            'FreePct'   = $freePct
+            'RunAs'     = $fsUser
+        }
+    } else {
+        $driveData = @{
+            'ShareName' = $ShareName
+            'RunAs'     = $fsUser
         }
     }
 
@@ -108,28 +121,36 @@ function Set-FileShareStatus {
         $UsePercent = $true
     )
 
-    if ($UsePercent = $true) {
-        if ($DriveInfo.FreePct -le $FreeSpaceErrorThreshold) {
-            $status = 'ERROR'
-        } elseif ($DriveInfo.FreePct -le $FreeSpaceWarningThreshold) {
-            $status = 'WARNING'
-        } elseif ($DriveInfo.FreePct -gt $FreeSpaceWarningThreshold) {
-            $status = 'OK'
+    if ($null -ne $DriveInfo) {
+        if ($UsePercent = $true) {
+            if ($DriveInfo.FreePct -le $FreeSpaceErrorThreshold -and $DriveInfo.FreePct -ne -1 -and $null -ne $DriveInfo.FreePct) {
+                $status = 'ERROR'
+            } elseif ($DriveInfo.FreePct -le $FreeSpaceWarningThreshold -and $DriveInfo.FreePct -ne -1 -and $null -ne $DriveInfo.FreePct) {
+                $status = 'WARNING'
+            } elseif ($DriveInfo.FreePct -gt $FreeSpaceWarningThreshold -and $DriveInfo.FreePct -ne -1 -and $null -ne $DriveInfo.FreePct) {
+                $status = 'OK'
+            } elseif ($DriveInfo.FreePct -eq -1 -or $null -eq $DriveInfo.FreePct) {
+                $status = 'CONNECT_FAILED'
+            } else {
+                $status = 'UNDECIDED'
+            }
+        } elseif ($UsePercent = $false) {
+            if ($DriveInfo.FreeGB -le $FreeSpaceErrorThreshold -and $DriveInfo.FreeGB -ne -1 -and $null -ne $DriveInfo.FreeGB) {
+                $status = 'ERROR'
+            } elseif ($DriveInfo.FreeGB -le $FreeSpaceWarningThreshold -and $DriveInfo.FreeGB -ne -1 -and $null -ne $DriveInfo.FreeGB) {
+                $status = 'WARNING'
+            } elseif ($DriveInfo.FreeGB -gt $FreeSpaceWarningThreshold -and $DriveInfo.FreeGB -ne -1 -and $null -ne $DriveInfo.FreeGB) {
+                $status = 'OK'
+            } elseif ($DriveInfo.FreeGB -eq -1 -or $null -eq $DriveInfo.FreeGB) {
+                $status = 'CONNECT_FAILED'
+            } else {
+                $status = 'UNDECIDED'
+            }
         } else {
-            $status = 'UNDECIDED'
-        }
-    } elseif ($UsePercent = $false) {
-        if ($DriveInfo.FreeGB -le $FreeSpaceErrorThreshold) {
-            $status = 'ERROR'
-        } elseif ($DriveInfo.FreeGB -le $FreeSpaceWarningThreshold) {
-            $status = 'WARNING'
-        } elseif ($DriveInfo.FreeGB -gt $FreeSpaceWarningThreshold) {
-            $status = 'OK'
-        } else {
-            $status = 'UNDECIDED'
+            $status = 'WEIRD_INPUT'
         }
     } else {
-        $status = 'WEIRD_INPUT'
+        $status = 'DRIVEINFO_EMPTY'
     }
 
     $DriveInfo['Status'] = $status -as [string]
@@ -142,18 +163,28 @@ function Send-FSPropertyBag {
         [Parameter()]
         [hashtable]
         $DriveInfo
+        , [Parameter()]
+        [string]
+        $UNCPath
     )
 
     $omApi = New-Object -ComObject 'MOM.ScriptAPI'
     $pb = $omApi.CreatePropertyBag()
 
-    $omApi.LogScriptEvent("FileShareSpace.ps1", 242, 0, ($DriveInfo | ConvertTo-Json))
+    if (($DriveInfo.Status -ne 'CONNECT_FAILED') -and ($DriveInfo.Status -ne 'DRIVEINFO_EMPTY') -and ($null -ne $DriveInfo)) {
+        $logMessage = "`n$($DriveInfo.ShareName)`nRunAs: $($DriveInfo.RunAs)`nDriveInfo:`n"
+        $logMessage += ($DriveInfo | ConvertTo-Json)
+    } else {
+        $logMessage = "`nProbing $UNCPath failed"
+    }
+    $omApi.LogScriptEvent('FileShareSpace.ps1', 242, 0, $logMessage)
+    $DriveInfo.Remove('RunAs') | Out-Null
 
     foreach ($key in $DriveInfo.Keys) {
         $pb.AddValue($key, $DriveInfo[$key])
     }
 
-    if ($host.Name -in @('Visual Studio Code Host')) {
+    if ($host.Name -in @('Visual Studio Code Host', 'Windows PowerShell ISE Host')) {
         $omApi.AddItem($pb)
         $omApi.ReturnItems()
     } else {
@@ -169,4 +200,4 @@ if ($UsePercent -eq 'false') {
 
 $driveInfo = Get-FileShareFreeSpace -UNCPath $UNCPath -ShareName $ShareName
 $driveInfo = Set-FileShareStatus -DriveInfo $driveInfo -FreeSpaceErrorThreshold $FreeSpaceErrorThreshold -FreeSpaceWarningThreshold $FreeSpaceWarningThreshold -UsePercent $usePct
-Send-FSPropertyBag -DriveInfo $driveInfo
+Send-FSPropertyBag -DriveInfo $driveInfo -UNCPath $UNCPath
