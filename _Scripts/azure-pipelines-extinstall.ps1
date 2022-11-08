963351b232dc2ec5f9fec2d859edbe0bdc3e25cf
Samuel Tegenfeldt <samuel.tegenfeldt@atea.se>
2022-11-08 11:33:58 +0100

    WMI was waaaay too slow

--- _Scripts/azure-pipelines-extinstall.ps1
+++ _Scripts/azure-pipelines-extinstall.ps1
@@ -1,16 +1,26 @@
 $vsaeUrl = "https://download.microsoft.com/download/4/6/e/46e6f247-d7fb-4923-84c7-38dffe6bc9be/VisualStudio2022AuthoringConsole_x64.msi"
 $vsaeFileName = "VisualStudio2022AuthoringConsole_x64.msi"
-# $vsaePackageName = "System Center Visual Studio Authoring Extensions"
+## $vsaePackageName = "System Center Visual Studio Authoring Extensions"
 
 Invoke-WebRequest -Uri $vsaeUrl -Method Get -OutFile $vsaeFileName -Verbose
 $msiFilePath = (Get-ChildItem -Path ".\$vsaeFileName").FullName
 
-$vsaeInstalled = Get-CimInstance -Query 'select Name from Win32_Product where Name like "%Visual Studio Autoring%"'
+# $vsaeInstalled = Get-CimInstance -Query 'select Name from Win32_Product where Name like "%Visual Studio Autoring%"'
+$vsaeInstalled = $false
+$installedSoftware = Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall"
+foreach ($obj in $InstalledSoftware) {
+    write-host $obj.GetValue('DisplayName')
+    # write-host " - " -NoNewline; write-host $obj.GetValue('DisplayVersion')
+    if ($obj.GetValue('DisplayName') -like '*Visual Studio Authoring*') {
+        $vsaeInstalled = $true
+    }
+}
 if (!$vsaeInstalled) {
     Write-Output "VSAE Not Installed!"
     Write-Output "Attempting install..."
     Start-Process msiexec.exe -Wait -ArgumentList "/I $msiFilePath /quiet"
-} else {
+}
+else {
     Write-Output "VSAE Already installed!"
 }
 
