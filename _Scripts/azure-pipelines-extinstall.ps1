ba2fa139d0d42adb909311f510b99076a394fa59
Samuel Tegenfeldt <stegenfeldt@users.noreply.github.com>
2023-01-31 11:26:17 +0100

    v1.0.3.400 (#55)
    * Added parameter UseServiceDisplayName for service discovery.
    
    * Changed space to tab in AteaWinSvcServiceDiscoveryDS.vbs.
    
    * Truncate File list in File Age Monitors
    Fixes #45
    
    * Set a proper  discovery interval
    
    * Updated error handling on Fileshare monitoring
    Fixes issue #47
    
    * Add parameter for number of files for alert in File Age Monitor
    Fixes #44
    
    * truncated description even harder
    
    * new version
    
    * Updated .gitignore.
    
    * Added monitor for service exist.
    
    * Request to merge small change to the windows file monitor  (#49)
    
    * Fixed issue with tuncated alert message
    * small formatting adjustment, realigning a few whitespaces
    Co-authored-by: Samuel Tegenfeldt <samuel.tegenfeldt@atea.se>
    
    * Stegenfeldt/issue51 (#52)
    
    Updated build test pipeline
    
    * ignoring vs2022 stuff
    
    * compiles alright after merge with pr
    
    * version bump to 1.0.3.400
    - pr 46-53
    
    * Changelog updated for 1.0.3.400 release
    
    ---------
    
    Co-authored-by: svantegraden <svante@snigel.eu>
    Co-authored-by: Svante Graden <svante.graden@atea.se>
    Co-authored-by: AndreasB-atea <65216950+AndreasB-atea@users.noreply.github.com>

--- _Scripts/azure-pipelines-extinstall.ps1
+++ _Scripts/azure-pipelines-extinstall.ps1
@@ -1,17 +1,26 @@
-$vsaeUrl = "https://download.microsoft.com/download/4/4/6/446B60D0-4409-4F94-9433-D83B3746A792/VisualStudioAuthoringConsole_x64.msi"
-$vsaeFileName = "VisualStudioAuthoringConsole_x64.msi"
-$vsaePackageName = "System Center Visual Studio Authoring Extensions"
+$vsaeUrl = "https://download.microsoft.com/download/4/6/e/46e6f247-d7fb-4923-84c7-38dffe6bc9be/VisualStudio2022AuthoringConsole_x64.msi"
+$vsaeFileName = "VisualStudio2022AuthoringConsole_x64.msi"
+## $vsaePackageName = "System Center Visual Studio Authoring Extensions"
 
 Invoke-WebRequest -Uri $vsaeUrl -Method Get -OutFile $vsaeFileName -Verbose
+$msiFilePath = (Get-ChildItem -Path ".\$vsaeFileName").FullName
 
-$vsaeInstalled = Get-Package -IncludeWindowsInstaller -Name $vsaePackageName -Verbose
+# $vsaeInstalled = Get-CimInstance -Query 'select Name from Win32_Product where Name like "%Visual Studio Autoring%"'
+$vsaeInstalled = $false
+$installedSoftware = Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall"
+foreach ($obj in $InstalledSoftware) {
+    if ($obj.GetValue('DisplayName') -like '*Visual Studio Authoring*') {
+        $vsaeInstalled = $true
+    }
+}
 if (!$vsaeInstalled) {
-    Write-Host "VSAE Not Installed!"
-    Write-Host "Attempting install..."
-    Install-Package -InputObject (Find-Package -Name ".\$vsaeFileName") -Verbose
-} else {
-    Write-Host "VSAE Already installed!"
+    Write-Output "VSAE Not Installed!"
+    Write-Output "Attempting install..."
+    Start-Process msiexec.exe -Wait -ArgumentList "/I $msiFilePath /quiet"
+}
+else {
+    Write-Output "VSAE Already installed!"
 }
 
-Write-Host "Copying SignKey..."
+Write-Output "Copying SignKey..."
 Copy-Item -Path $env:DOWNLOADSECUREFILE_SECUREFILEPATH -Destination ".\" -Verbose
